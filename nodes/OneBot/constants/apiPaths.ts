ceb18c46eb370ea6a54b2a3a593d2c22388b4a00
final <3486159271@qq.com>
2025-12-07 10:59:54 +0800

    feat: 更新 apiPaths.ts 中的接口路径常量

--- nodes/OneBot/constants/apiPaths.ts
+++ nodes/OneBot/constants/apiPaths.ts
@@ -2,7 +2,17 @@
  * 接口路径常量（统一管理所有接口端点）
  */
 export const API_PATHS = {
-  // 点赞接口
+
+	// 获取好友列表
+	getFriendList: 'get_friend_list',
+
+	// 获取群列表
+	getGroupList: 'get_group_list',
+
+	// 获取群成员列表
+	getGroupMemberList: 'get_group_member_list',
+
+  // 点赞
   sendLike: 'send_like',
 
 };
