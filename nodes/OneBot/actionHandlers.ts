31f28a49e48822dddf8bd84b82e8d59b135439ed
final <3486159271@qq.com>
2026-03-22 22:32:51 +0800

    refactor(core): clean unused params in operation handlers

--- nodes/OneBot/actionHandlers.ts
+++ nodes/OneBot/actionHandlers.ts
@@ -61,6 +61,7 @@ const messageForwardHandlers: Record<string, ForwardHandler> = {
 
 const friendOperationHandlers: Record<string, OperationHandler> = {
 	get_friend_list: async function (this: IExecuteFunctions, index: number) {
+		void index;
 		return apiRequest.call(this, 'GET', 'get_friend_list');
 	},
 	get_stranger_info: async function (this: IExecuteFunctions, index: number) {
@@ -86,6 +87,7 @@ const groupOperationHandlers: Record<string, OperationHandler> = {
 		return apiRequest.call(this, 'POST', 'get_group_info', body);
 	},
 	get_group_list: async function (this: IExecuteFunctions, index: number) {
+		void index;
 		return apiRequest.call(this, 'GET', 'get_group_list');
 	},
 	get_group_member_info: async function (this: IExecuteFunctions, index: number) {
@@ -113,13 +115,16 @@ const groupOperationHandlers: Record<string, OperationHandler> = {
 const genericOperationHandlers: Record<string, OperationHandler> = {
 	// Bot 相关操作
 	get_login_info: async function (this: IExecuteFunctions, index: number) {
+		void index;
 		return apiRequest.call(this, 'GET', 'get_login_info');
 	},
 	// Misc 相关操作
 	get_status: async function (this: IExecuteFunctions, index: number) {
+		void index;
 		return apiRequest.call(this, 'GET', 'get_status');
 	},
 	get_version_info: async function (this: IExecuteFunctions, index: number) {
+		void index;
 		return apiRequest.call(this, 'GET', 'get_version_info');
 	},
 };
@@ -185,7 +190,7 @@ export async function executeGenericOperation(
 	operation: string,
 	index: number,
 ): Promise<IDataObject> {
-	let body: IDataObject = {};
+	const body: IDataObject = {};
 
 	switch (operation) {
 		case 'get_stranger_info':
